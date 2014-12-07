af0fdbcf326adf6e6c4181e366ba502b8a68468b
Derrick Liang <liangderrick@yahoo.com>
2014-12-06 17:41:03 -0800

    admins view all purchases

--- Web/viewpurchases.php
+++ Web/viewpurchases.php
@@ -4,6 +4,56 @@
 
 	$custID = $_SESSION["customerID"];
 
+	function printAllPurchases() {
+		global $conn;
+
+		$query = "SELECT pur.purchaseID,pur.productID,pur.customerID,cust.name as cname,prod.name as pname,pur.buyquantity,pur.datetime
+			FROM purchases pur,product prod,customer cust 
+			WHERE pur.productID=prod.productID AND cust.customerID=pur.customerID";
+		$result = mysqli_query($conn,$query);
+
+		if (mysqli_num_rows($result) == 0) {
+			echo "<h3>No purchases have been made.</h3>";
+		}
+		else {
+			echo "<h3>Your purchases</h3>";
+		
+			echo "
+			<table border='2'>
+				<thead>
+					<tr>
+						<th>Purchase ID</th>
+						<th>Customer ID</th>
+						<th>Customer name</th>
+						<th>Product ID</th>
+						<th>Product Name</th>
+						<th>Quantity</th>
+						<th>Purchase Date</th>
+					</tr>
+				</thead>
+			";
+
+			while ($data = mysqli_fetch_array($result)) {
+				echo "<tr>";
+
+				echo "<td>" . $data['purchaseID'] . "</td>";
+				echo "<td>" . $data['customerID'] . "</td>";
+				echo "<td>" . $data['cname'] . "</td>";
+				echo "<td>" . $data['productID'] . "</td>";
+				echo "<td>" . $data['pname'] . "</td>";
+				echo "<td>" . $data['buyquantity'] . "</td>";
+				echo "<td>" . $data['datetime'] . "</td>";
+
+
+				echo "</tr>";
+			}
+
+			echo "</table>";
+
+		}
+
+	}
+
 	function printPurchases() {
 		global $custID,$conn;
 
@@ -62,7 +112,13 @@
 	<body>
 		<?php 
 			require_once("navbar.php"); 
-			printPurchases();
+
+			if ($_SESSION['role'] == 'A' || $_SESSION['role'] == 'M'){
+				printAllPurchases();
+			}
+			else {
+				printPurchases();
+			}
 		?>
 	</body>
 </html>
